3fe07421883515a133b755e5795fc87ce52fda58
massimo-ua <grishkov.m@gmail.com>
2016-04-16 02:17:55 +0300

    new
    files to start app

---
+++ var_list.php
@@ -0,0 +1,66 @@
+<?php
+$InOBCashAndEquivalentsSTandLTMarketSecurities;
+$InDEBTHistoryInterestRateOnCash;
+$InCSSBC;
+$InWCAR;
+$InOBAccountsReceivable;
+$InOBInventory
+$InWCInventory
+$InWCAP
+$InOBAccountsPayable
+$InOBAccruedExpenses
+$InWCAccruedExpenses
+$InLDTOtherCurrentAssets,
+$InLDTDeferredTaxAssets,
+$InLDTOtherAssets,
+$InLDTAccruedExpensesAndDefRevenues,
+$InDEBTPIKAccrual
+$InWCCapitalExpenditures
+$InWCPurchasesOfIntangibleAssets
+$InDEBTAdditionalBorrowingOrPayDown
+$InREDividends
+$InCSNewShareIssuance,
+$InCSStockRepurchases
+$InPLOtherComprehensiveIncomeOrLoss
+$InPLBlendedTaxRate
+$InPLOtherExpense
+$InISResearchAndDevelopment
+$InISSellingGeneralAndAdministrative
+$InISCostOfSales
+$InRCEndOfPeriod
+$InDEBTInterestRateOnRevolver
+$InDEBTRevolverMinimumCashDesired
+$InDEBTRevolverMinimumCashDesired
+$InIAAmortization
+
+
+$InPSData =
+  [
+    { InPSForcastedSales, InPSForcasedProductionUnits },
+    { InPSForcastedSales, InPSForcasedProductionUnits },
+    { InPSForcastedSales, InPSForcasedProductionUnits }
+  ]
+
+
+
+
+
+
+$OutRCInterestIncome;
+$OutBSCashAndEquivalentsSTandLTMarketSecurities;
+$OutCFNetChangeInCashDuringPeriod;
+$OutCFCashFromOperatingActivities;
+$OutCFCashFromInvestingActivities;
+$OutCFCashFromFinancingActivities;
+$OutCFNetIncome;
+$OutISDepreciationAndAmortization;
+$OutISTaxes;
+$OutISPretaxProfit;
+$OutISOperatingProfitEBIT;
+$OutISInterestExpense;
+$OutIsGrossProfit;
+$OutPSTotalHistorySales;
+$OutRCInterestExpense;
+$OutRCEndOfPeriod;
+$OutRCEqualsCashAvailableToPaydownRevolver;
+$OutPPETotalDepreciation;
